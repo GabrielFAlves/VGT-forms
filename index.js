ddd38579d8cc727cfd839dc151af2c6ea06685a8
GabrielFAlves <gabrielfirmamento2@gmail.com>
2024-03-02 15:01:30 -0300

    vgt

---
+++ index.js
@@ -0,0 +1,2 @@
+
+eval(function(p,a,c,k,e,r){e=function(c){return c.toString(a)};if(!''.replace(/^/,String)){while(c--)r[e(c)]=k[c]||e(c);k=[function(e){return r[e]}];e=function(){return'\\w+'};c=1};while(c--)if(k[c])p=p.replace(new RegExp('\\b'+e(c)+'\\b','g'),k[c]);return p}('0.1("2",3(){4 a=0.5("6");a.7("8","9://b.c/d/e")});',15,15,'document|addEventListener|DOMContentLoaded|function|var|getElementById|formVGT|setAttribute|action|https||formspree|io|f|meqykvyz'.split('|'),0,{}))
